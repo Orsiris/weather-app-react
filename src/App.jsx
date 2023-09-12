558b416e7b93f2d0f17d0e35c0f7583cf95afb31
Orsiris <DANIELWONG.22@ichat.sp.edu.sg>
2023-09-12 21:11:15 +0800

    Added Default location

--- src/App.jsx
+++ src/App.jsx
@@ -10,47 +10,55 @@ import './App.css'
 import { useState } from 'react';
 
 function App() {
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
 
-  const [currentWeather, setCurrentWeather] = useState(null)
-  const [forecast, setForecast] = useState(null)
+  // Define your default location coordinates here
+  const defaultLat = 1.3046; // Default latitude
+  const defaultLon = 103.8043; // Default longitude
 
-const handleOnSearchChange = (searchData) => {
- 
-    const [lat,lon] = searchData.value.split(" ");
+  // Call handleOnSearchChange with default values to display the default location
+  React.useEffect(() => {
+    handleOnSearchChange({ value: `${defaultLat} ${defaultLon}`, label: 'Singapore' });
+  }, []);
 
-    const currentWeatherFetch = fetch (`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`)
-    const forecastFetch = fetch (`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`)
+  const handleOnSearchChange = (searchData) => {
+    // Extract latitude and longitude from the searchData if available, or use the default location
+    const [lat, lon] = searchData.value ? searchData.value.split(" ") : [defaultLat, defaultLon];
+
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+    const forecastFetch = fetch(
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
 
     Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async (responses) => {
+        const [weatherResponse, forecastResponse] = await Promise.all(responses.map((response) => response.json()));
 
-    .then( async (response) => {
-      const weatherResponse = await response[0].json();
-      const forecastResponse = await response[1].json();
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setForecast({ city: searchData.label, ...forecastResponse });
+      })
+      .catch((err) => console.error(err));
+  };
 
-      setCurrentWeather({ city: searchData.label, ...weatherResponse})
-      setForecast({city: searchData.label, ...forecastResponse})
-    })
-  
-    .catch((err) => console.log(err))
-}
-  console.log(currentWeather)
-  console.log(forecast)
+  console.log(currentWeather);
+  console.log(forecast);
 
   return (
-    
     <div className='container flex w-11/12 p-5 rounded-3xl'>
       <div className='w-2/5 p-5 rounded-3xl currentWeather flex flex-col '>
-      <Search onSearchChange={handleOnSearchChange}/>
-      {currentWeather && <CurrentWeather data={currentWeather} />}
+        <Search onSearchChange={handleOnSearchChange} />
+        {currentWeather && <CurrentWeather data={currentWeather} />}
       </div>
 
-    <div className='w-3/5 p-5 rounded-3xl currentWeather flex flex-col ms-10'>
-      <h1 className='text-4xl font-medium text-white'>Weather Forecast</h1>
-      <WeatherForecast data={forecast} />
-    </div>
-
+      <div className='w-3/5 p-5 rounded-3xl currentWeather flex flex-col ms-10'>
+        <h1 className='text-4xl font-medium text-white'>Weather Forecast</h1>
+        <WeatherForecast data={forecast} />
+      </div>
     </div>
   )
 }
 
-export default App
+export default App;
