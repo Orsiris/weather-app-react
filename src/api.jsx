ac9ea713140a9e1fd4056584b52f20e1a17f1408
Orsiris <DANIELWONG.22@ichat.sp.edu.sg>
2023-09-19 14:57:33 +0800

    changed env again

--- src/api.jsx
+++ src/api.jsx
@@ -1,21 +1,28 @@
+const geoApiKey = import.meta.env.VITE_GEO_API_KEY;
+const geoApiHost = import.meta.env.VITE_GEO_API_HOST;
+const weatherApiKey = import.meta.env.VITE_WEATHER_API_KEY;
+const weatherApiHost = import.meta.env.VITE_WEATHER_API_HOST;
 
-const ApiKey = proccess.env.REACT_APP_API_KEY
-const ApiHost = proccess.env.REACT_APP_WEATHER_API_HOST
 
-
-
-
-export const weatherApiOptions = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': ApiKey,
-		'X-RapidAPI-Host': ApiHost,
-	}
+export const geoApiOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': geoApiKey,
+    'X-RapidAPI-Host': geoApiHost,
+  },
 };
 
+export const weatherApiOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': weatherApiKey,
+    'X-RapidAPI-Host': weatherApiHost,
+  },
+};
 
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
 
-export const WEATHER_API_URL = proccess.env.REACT_APP_WEATHER_API_URL
+export const WEATHER_API_URL = "https://weatherapi-com.p.rapidapi.com"
 
 
  
